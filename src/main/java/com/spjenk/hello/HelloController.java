a952e454eebeacf52d0248ab732cd54857c95e37
cloudtechmasters <68885738+cloudtechmasters@users.noreply.github.com>
2020-08-30 21:50:47 +0530

    Update HelloController.java

--- src/main/java/com/spjenk/hello/HelloController.java
+++ src/main/java/com/spjenk/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "Welcome to Cloudtechmasters!!!!!!";
+        return "Welcome to Cloudtechmasters version:1";
     }
 }
